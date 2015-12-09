1b42e16e9e15d7e526c40541bcd95cd366bae68e
Florent Georges <fgeorges@gmail.com>
2015-12-10 00:43:14 +0100

    Add optional option to compile only one page.

--- website/scripts/compile-all.sh
+++ website/scripts/compile-all.sh
@@ -3,8 +3,22 @@
 # temporary marked's output file
 tmp=tmp-marked.out
 
+sources=../src/*.md
+
+if [ -n "$1" ]; then
+    sources="../src/${1}.md"
+    if [ \! -f "$sources" ]; then
+	echo "Source Markdown file does not exist: $sources"
+	exit 1
+    fi
+    if [ -n "$2" ]; then
+	echo "Extra option, only one (optional) source file permitted: $2"
+	exit 1
+    fi
+fi
+
 # loop over all markdown files
-for md in ../src/*.md
+for md in $sources
 do
     # the entry name, with no extension
     name=`echo $md | sed s/\\\\.\\\\.\\\\/src\\\\/// | sed s/.md//`
